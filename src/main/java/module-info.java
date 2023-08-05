598aee0b6756dc1bee2f5ead636f833b3aabaeca
Lincoln Hubbard <hubbard.lincoln@gmail.com>
2023-08-04 17:07:18 -0700

    Modify module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,5 +8,8 @@ module com.hubbard.appointmentapp {
     opens com.hubbard.appointmentapp to javafx.fxml;
     exports com.hubbard.appointmentapp;
     exports com.hubbard.appointmentapp.controller;
+    exports com.hubbard.appointmentapp.model;
+    exports com.hubbard.appointmentapp.dao;
     opens com.hubbard.appointmentapp.controller to javafx.fxml;
+    opens com.hubbard.appointmentapp.dao to javafx.fxml;
 }
